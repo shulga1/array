404ad3d346099718985f197f731e1e68b6f0005e
Yuriy Shulga <jsshu1ga@gmail.com>
2020-09-25 23:47:02 +0300

    fix algorithm bubbleSort

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-param-reassign */
 const bubbleSort = (numbers) => {
   for (let i = 0; i < numbers.length - 1; i += 1) {
-    for (let j = i + 1; j < numbers.length; j += 1) {
-      if (numbers[i] > numbers[j]) {
-        const temp = numbers[i];
-        numbers[i] = numbers[j];
-        numbers[j] = temp;
+    for (let j = 0; j < numbers.length - i; j += 1) {
+      if (numbers[j] > numbers[j + 1]) {
+        const temp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = temp;
       }
     }
   }
